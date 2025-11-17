0c4e8befe22d2b5c7eb946bd752dbab89cef3aea
DangHuy1302 <lphan7024@gmail.com>
2025-11-17 18:07:54 +0700

    Final Project: Personalize portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,8 +59,8 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            document.title = "Portfolio | Dang Huy";
+            $("#favicon").attr("href", "assets/images/HuyHero.png");
         }
         else {
             document.title = "Come Back To Portfolio";
@@ -110,7 +110,7 @@ function showProjects(projects) {
     projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
         projectHTML += `
         <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="./assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
